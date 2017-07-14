8c98e455a713bb24e265e0bb9b565c28ad80ce0b
Charlotte Soneson <charlottesoneson@gmail.com>
2017-07-14 10:05:34 +0200

    Cleaned up shiny code, allow time stamp in file names.

--- shiny-download/shiny-download.R
+++ shiny-download/shiny-download.R
@@ -1,9 +1,9 @@
-library(shiny)
-library(shinydashboard)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(DT)
+suppressPackageStartupMessages(library(shiny))
+suppressPackageStartupMessages(library(shinydashboard))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(DT))
 
 scrna_download_shiny <- function(data_directory, top_url) {
   ## ----------------------------------------------------------------------- ##
@@ -49,83 +49,101 @@ scrna_download_shiny <- function(data_directory, top_url) {
           tabPanel("Excluded samples",
                    includeMarkdown("/home/Shared_taupo/data/seq/conquer/database/shiny-download/excluded_samples.md"),
                    value = "excludedsamples"),
-          
+
           tabPanel("Tutorial",
                    includeMarkdown("/home/Shared_taupo/data/seq/conquer/database/shiny-download/tutorial.md"),
                    value = "tutorial"),
           
           selected = "select_dataset"
-            )
-            ))
-      )
+        )
+      ))
+    )
   
   ## ----------------------------------------------------------------------- ##
   ##                           Define server                                 ##
   ## ----------------------------------------------------------------------- ##
   
   server_function <- function(input, output, session) {
+    
     withProgress(message = "Retrieving data...", value = 0, {
-      ## MultiAssayExperiment
-      maestbl <- file.info(list.files(paste0(data_directory, "/data-mae"), pattern = "\\.rds$",
-                                      full.names = TRUE))
-      rownames(maestbl) <- gsub(data_directory, top_url, rownames(maestbl))
+      
+      ## ----------------- MultiAssayExperiment table ----------------------- ##
+      maestbl <- file.info(list.files(paste0(data_directory, "/data-mae"), 
+                                      pattern = "\\.rds$", full.names = TRUE))
       colnames(maestbl) <- paste0("mae_", colnames(maestbl))
-      maestbl$mae_link <- rownames(maestbl)
-      rownames(maestbl) <- gsub("\\.rds$", "", basename(rownames(maestbl)))
+      maestbl$mae_link <- gsub(data_directory, top_url, rownames(maestbl))
+      maestbl$dataset <- gsub("(_20[0-9]{6})*\\.rds$", "", basename(rownames(maestbl)))
+      maestbl <- maestbl %>% dplyr::group_by(dataset) %>% dplyr::arrange(mae_mtime) %>%
+        dplyr::summarise_all(funs(last)) %>% dplyr::ungroup() %>% 
+        as.data.frame()
 
-      ## MultiQC
-      # multiqctbl <- file.info(system(paste0("ls ", data_directory, "/data-processed/*/multiqc/*_multiqc_report.html"), 
-      #                                intern = TRUE))
+      ## --------------------- MultiQC table -------------------------------- ##
       multiqctbl <- file.info(list.files(paste0(data_directory, "/report-multiqc"),
-                                         pattern = "_multiqc_report.html$",
+                                         pattern = "_multiqc_report(_20[0-9]{6})*.html$",
                                          full.names = TRUE))
-      rownames(multiqctbl) <- gsub(data_directory, top_url, rownames(multiqctbl))
       colnames(multiqctbl) <- paste0("multiqc_", colnames(multiqctbl))
-      multiqctbl$multiqc_link <- rownames(multiqctbl)
-      rownames(multiqctbl) <- gsub("_multiqc_report.html$", "", basename(rownames(multiqctbl)))
-      
-      ## scater
-      scatertbl <- file.info(list.files(paste0(data_directory, "/report-scater"), 
-                                        pattern = "_scater.html$",
-                                        full.names = TRUE))
-      rownames(scatertbl) <- gsub(data_directory, top_url, rownames(scatertbl))
-      colnames(scatertbl) <- paste0("scater_", colnames(scatertbl))
-      scatertbl$scater_link <- rownames(scatertbl)
-      rownames(scatertbl) <- gsub("_scater.html$", "", basename(rownames(scatertbl)))
+      multiqctbl$multiqc_link <- gsub(data_directory, top_url, rownames(multiqctbl))
+      multiqctbl$dataset <- gsub("_multiqc_report(_20[0-9]{6})*\\.html$", "", 
+                                 basename(rownames(multiqctbl)))
+      multiqctbl <- multiqctbl %>% dplyr::group_by(dataset) %>% 
+        dplyr::arrange(multiqc_mtime) %>% dplyr::summarise_all(funs(last)) %>% 
+        dplyr::ungroup() %>% as.data.frame()
 
-      ## Salmon
-      salmontbl <- file.info(system(paste0("ls ", data_directory, "/data-processed/*/*_salmon.tar.gz"), 
+      ## --------------------- scater table --------------------------------- ##
+      scatertbl <- file.info(list.files(paste0(data_directory, "/report-scater"), 
+                                        pattern = "_scater(_20[0-9]{6})*.html$",
+                                        full.names = TRUE))
+      colnames(scatertbl) <- paste0("scater_", colnames(scatertbl))
+      scatertbl$scater_link <- gsub(data_directory, top_url, rownames(scatertbl))
+      scatertbl$dataset <- gsub("_scater(_20[0-9]{6})*\\.html$", "", 
+                                basename(rownames(scatertbl)))
+      scatertbl <- scatertbl %>% dplyr::group_by(dataset) %>% 
+        dplyr::arrange(scater_mtime) %>% dplyr::summarise_all(funs(last)) %>% 
+        dplyr::ungroup() %>% as.data.frame()
+      
+      ## --------------------- Salmon table --------------------------------- ##
+      salmontbl <- file.info(system(paste0("ls ", data_directory, 
+                                           "/data-processed/*/*_salmon*.tar.gz"), 
                                      intern = TRUE))
-      # salmontbl <- file.info(list.files(paste0(data_directory, "/data-processed"), 
-      #                                   pattern = "_salmon.tar.gz$",
-      #                                   full.names = TRUE, recursive = TRUE))
-      rownames(salmontbl) <- gsub(data_directory, top_url, rownames(salmontbl))
       colnames(salmontbl) <- paste0("salmon_", colnames(salmontbl))
-      salmontbl$salmon_link <- rownames(salmontbl)
-      rownames(salmontbl) <- gsub("_salmon.tar.gz$", "", basename(rownames(salmontbl)))
+      salmontbl$salmon_link <- gsub(data_directory, top_url, rownames(salmontbl))
+      salmontbl$dataset <- gsub("_salmon(_20[0-9]{6})*\\.tar.gz$", "", 
+                                basename(rownames(salmontbl)))
+      salmontbl <- salmontbl %>% dplyr::group_by(dataset) %>% 
+        dplyr::arrange(salmon_mtime) %>% dplyr::summarise_all(funs(last)) %>% 
+        dplyr::ungroup() %>% as.data.frame()
       
-      dtbl <- merge(merge(maestbl, multiqctbl, by = 0, all = TRUE),
-                    merge(scatertbl, salmontbl, by = 0, all = TRUE),
-                    by = "Row.names", all = TRUE)
-      
-      ## Add some basic information about the data set
-      dss <- list.files(paste0(data_directory, "/data-processed"), full.names = TRUE, recursive = FALSE)
+      ## ---------------------- Merge tables -------------------------------- ##
+      dtbl <- maestbl %>% dplyr::full_join(multiqctbl, by = "dataset") %>%
+        dplyr::full_join(scatertbl, by = "dataset") %>%
+        dplyr::full_join(salmontbl, by = "dataset")
+
+      ## ----------- Add some basic information about the data sets --------- ##
+      dss <- list.files(paste0(data_directory, "/data-processed"), 
+                        full.names = TRUE, recursive = FALSE) ## datasets
       names(dss) <- basename(dss)
       basic_info <- data.frame(t(sapply(dss, function(ds) {
-        fn <- paste0(ds, "/dataset_info.txt")
+        fn <- file.info(list.files(ds, pattern = "dataset_info", 
+                                   full.names = TRUE)) %>%
+          tibble::rownames_to_column(var = "file") %>%
+          dplyr::arrange(mtime) %>% dplyr::summarise_all(funs(last)) %>% 
+          as.data.frame()
+        fn <- fn$file
         if (file.exists(fn)) {
           fn <- read.delim(fn, header = FALSE, row.names = 1, as.is = TRUE)
           c(organism = fn["organism", 1], ncells = fn["nsamples", 1],
-            pmid = fn["PMID", 1], datalink = fn["datalink", 1], ID = fn["shortname", 1])
+            pmid = fn["PMID", 1], datalink = fn["datalink", 1], 
+            ID = fn["shortname", 1])
         } else {
           c(organism = NA, ncells = NA, pmid = NA, datalink = NA, ID = NA)
         }
-      })), stringsAsFactors = FALSE)
+      })), stringsAsFactors = FALSE) %>% 
+        tibble::rownames_to_column(var = "dataset")
 
-      dtbl <- merge(dtbl, basic_info,
-                    by.x = "Row.names", by.y = 0, all = TRUE)
+      dtbl <- dtbl %>% dplyr::full_join(basic_info, by = "dataset")
     })
 
+    ## ----------------- Help functions to create links --------------------- ##
     create_link <- function(val, title, chdate) {
       s <- sprintf('<a href="%s" target="_blank" class="btn btn-primary">Download %s</a> (%s)', 
                    val, title, chdate)
@@ -148,16 +166,16 @@ scrna_download_shiny <- function(data_directory, top_url) {
       paste0(s1, s2)
     }
     
+    ## --------------------- Final data table ------------------------------- ##
     output$dt_datasets <- DT::renderDataTable({
-      dtbl$data_set <- create_link_2(dtbl$datalink, dtbl$Row.names, dtbl$pmid)
-##      dtbl$data_set <- dtbl$Row.names
+      dtbl$`Data set` <- create_link_2(dtbl$datalink, dtbl$dataset, dtbl$pmid)
       dtbl$MultiAssayExperiment <- create_link(dtbl$mae_link, ".rds", as.Date(dtbl$mae_mtime))
-      dtbl$MultiQC_html <- create_link(dtbl$multiqc_link, ".html", as.Date(dtbl$multiqc_mtime))
-      dtbl$scater_html <- create_link(dtbl$scater_link, ".html", as.Date(dtbl$scater_mtime))
-      dtbl$salmon_tar <- create_link(dtbl$salmon_link, ".tar.gz", as.Date(dtbl$salmon_mtime))
+      dtbl$`MultiQC report` <- create_link(dtbl$multiqc_link, ".html", as.Date(dtbl$multiqc_mtime))
+      dtbl$`scater report` <- create_link(dtbl$scater_link, ".html", as.Date(dtbl$scater_mtime))
+      dtbl$`salmon archive` <- create_link(dtbl$salmon_link, ".tar.gz", as.Date(dtbl$salmon_mtime))
       dtbl$ncells <- as.numeric(as.character(dtbl$ncells))
-      return(dtbl[, c("data_set", "ID", "organism", "ncells", "MultiAssayExperiment", 
-                      "MultiQC_html", "scater_html", "salmon_tar")])
+      return(dtbl[, c("Data set", "ID", "organism", "ncells", "MultiAssayExperiment", 
+                      "MultiQC report", "scater report", "salmon archive")])
     }, escape = FALSE)   
 
   }
